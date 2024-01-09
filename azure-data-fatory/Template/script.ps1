e12ce1b1e0906e385689f9dfadf3a597d7bb73b9
rahul <azuredevopst@gmail.com>
2024-01-09 11:34:35 +0530

    ./fuction code updated

---
+++ azure-data-fatory/Template/script.ps1
@@ -0,0 +1,23 @@
+# Install Azure PowerShell module
+Install-Module -Name Az -Force -AllowClobber
+
+# Import Azure PowerShell module
+Import-Module Az
+
+# Authenticate to Azure
+Connect-AzAccount -ServicePrincipal creds: ${secrets.AZURE_CREDENTIALS}
+# Define variables
+$resourceGroupName = "project-dev"
+$dataFactoryName = "myappadf"
+$pipelineName = "ds_raiscopy"
+$datasetName = "ds_azurcloud"
+$linkedServiceName = "ds_azuresqls"
+
+# Delete Pipeline
+Remove-AzDataFactoryPipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $pipelineName -Force
+
+# Delete Dataset
+Remove-AzDataFactoryDataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $datasetName -Force
+
+# Delete Linked Service
+Remove-AzDataFactoryLinkedService -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $linkedServiceName -Force
