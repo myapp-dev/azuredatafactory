ae8a6897530549d59efbc68879ab3efa2e224236
rahul <azuredevopst@gmail.com>
2024-01-09 12:12:35 +0530

    ./fuction code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -1,33 +1,18 @@
 
-# Install Azure PowerShell module
-Install-Module -Name Az -Force -AllowClobber
 
-# Import Azure PowerShell module
-Import-Module Az
+# Login to Azure
+Connect-AzAccount
 
-# Authenticate to Azure
-$azCredentials = $secrets.AZURE_CREDENTIALS
-$servicePrincipal = New-Object PSCredential($azCredentials.ApplicationId, (ConvertTo-SecureString $azCredentials.Secret -AsPlainText -Force))
-Connect-AzAccount -ServicePrincipal -Credential $servicePrincipal -Tenant $azCredentials.TenantId
-
-# Set Azure context
-Set-AzContext -SubscriptionId "6712c173-cfc7-47a7-b8ee-cf07ef93f18d"
-
-# Define variables
+# Specify resource group, data factory name, pipeline name, and dataset name
 $resourceGroupName = "project-dev"
 $dataFactoryName = "myappadf"
 $pipelineName = "ds_raiscopy"
 $datasetName = "ds_azurcloud"
-$linkedServiceName = "ds_azuresqls"
 
-# Specify API version
-$apiVersion = "2018-06-01"
+# Delete the pipeline
+Remove-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $pipelineName -Force
 
-# Delete Pipeline
-Remove-AzDataFactoryPipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $pipelineName -Force -ApiVersion $apiVersion
+# Delete the dataset
+Remove-AzDataFactoryV2Dataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $datasetName -Force
 
-# Delete Dataset
-Remove-AzDataFactoryDataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $datasetName -Force -ApiVersion $apiVersion
-
-# Delete Linked Service
-Remove-AzDataFactoryLinkedService -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $linkedServiceName -Force -ApiVersion $apiVersion
+Write-Output "Pipeline and dataset deleted successfully."
