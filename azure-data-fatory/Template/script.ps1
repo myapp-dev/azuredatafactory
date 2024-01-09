70778b3dd01a9485c6ba1296b779de71ee519d97
rahul <azuredevopst@gmail.com>
2024-01-09 15:10:55 +0530

    ./fuction code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -1,21 +1,45 @@
+$DataFactoryName = "myappdev"
+$ResourceGroupName = "project-dev"
 
+# Deleted resources
+# pipelines
+Write-Host "Getting pipelines"
+$pipelinesADF = Get-SortedPipelines -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
+$pipelinesTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/pipelines" }
+$pipelinesNames = $pipelinesTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
+$deletedpipelines = $pipelinesADF | Where-Object { $pipelinesNames -notcontains $_.Name }
 
-Import-Module Az.Accounts  # Import Azure modules for authentication
-Import-Module Az.DataFactory # Import Azure Data Factory module
+# datasets
+Write-Host "Getting datasets"
+$datasetsADF = Get-AzDataFactoryV2Dataset -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
+$datasetsTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/datasets" }
+$datasetsNames = $datasetsTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40) }
+$deleteddataset = $datasetsADF | Where-Object { $datasetsNames -notcontains $_.Name }
 
-# Login to Azure
-Connect-AzAccount
+# linkedservices
+Write-Host "Getting linked services"
+$linkedservicesADF = Get-SortedLinkedServices -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
+$linkedservicesTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/linkedservices" }
+$linkedservicesNames = $linkedservicesTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
+$deletedlinkedservices = $linkedservicesADF | Where-Object { $linkedservicesNames -notcontains $_.Name }
 
-# Specify resource group, data factory name, pipeline name, and dataset name
-$resourceGroupName = "project-dev"
-$dataFactoryName = "myappadf"
-$pipelineName = "ds_raiscopy"
-$datasetName = "ds_azurcloud"
+# Delete resources
+Write-Host "Deleting pipelines"
+$deletedpipelines | ForEach-Object { 
+    Write-Host "Deleting pipeline " $_.Name
+    Remove-AzDataFactoryV2Pipeline -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
+}
 
-# Delete the pipeline
-Remove-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $pipelineName -Force
+Write-Host "Deleting datasets"
+$deleteddataset | ForEach-Object { 
+    Write-Host "Deleting dataset " $_.Name
+    Remove-AzDataFactoryV2Dataset -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
+}
 
-# Delete the dataset
-Remove-AzDataFactoryV2Dataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $datasetName -Force
+Write-Host "Deleting linked services"
+$deletedlinkedservices | ForEach-Object { 
+    Write-Host "Deleting Linked Service " $_.Name
+    Remove-AzDataFactoryV2LinkedService -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
+}
 
-Write-Output "Pipeline and dataset deleted successfully."
+# ...
