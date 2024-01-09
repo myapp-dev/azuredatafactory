a355bd3bee1c141072bc9d98b6f6bd13d41e9d20
rahul <azuredevopst@gmail.com>
2024-01-09 22:18:28 +0530

    ./fuction code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -1,45 +1,17 @@
-$DataFactoryName = "myappdev"
-$ResourceGroupName = "project-dev"
+param(
+    [Switch]$Fail, 
+    [Switch]$ExitCode, 
+    $Parameter1
+)
 
-# Deleted resources
-# pipelines
-Write-Host "Getting pipelines"
-$pipelinesADF = Get-SortedPipelines -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
-$pipelinesTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/pipelines" }
-$pipelinesNames = $pipelinesTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
-$deletedpipelines = $pipelinesADF | Where-Object { $pipelinesNames -notcontains $_.Name }
-
-# datasets
-Write-Host "Getting datasets"
-$datasetsADF = Get-AzDataFactoryV2Dataset -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
-$datasetsTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/datasets" }
-$datasetsNames = $datasetsTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40) }
-$deleteddataset = $datasetsADF | Where-Object { $datasetsNames -notcontains $_.Name }
-
-# linkedservices
-Write-Host "Getting linked services"
-$linkedservicesADF = Get-SortedLinkedServices -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
-$linkedservicesTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/linkedservices" }
-$linkedservicesNames = $linkedservicesTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
-$deletedlinkedservices = $linkedservicesADF | Where-Object { $linkedservicesNames -notcontains $_.Name }
-
-# Delete resources
-Write-Host "Deleting pipelines"
-$deletedpipelines | ForEach-Object { 
-    Write-Host "Deleting pipeline " $_.Name
-    Remove-AzDataFactoryV2Pipeline -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
+if ($Fail) {
+    throw "This script fails!"
 }
 
-Write-Host "Deleting datasets"
-$deleteddataset | ForEach-Object { 
-    Write-Host "Deleting dataset " $_.Name
-    Remove-AzDataFactoryV2Dataset -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
+if ($ExitCode) {
+    exit 5
 }
 
-Write-Host "Deleting linked services"
-$deletedlinkedservices | ForEach-Object { 
-    Write-Host "Deleting Linked Service " $_.Name
-    Remove-AzDataFactoryV2LinkedService -Name $_.Name -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Force 
-}
-
-# ...
+$Env:MyVariable
+$Parameter1
+$PSVersionTable
