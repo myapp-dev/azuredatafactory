9a4d15d66dff027ac4edc31b1d48ff837735f6f9
rahul <azuredevopst@gmail.com>
2024-01-10 12:35:53 +0530

    ./fuction code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -1,17 +1,39 @@
-param(
-    [Switch]$Fail, 
-    [Switch]$ExitCode, 
-    $Parameter1
-)
+$personalToken = "AzureDevOpsPersonalToken"
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalToken)"))
+$header = @{authorization = "Basic $token"}
 
-if ($Fail) {
-    throw "This script fails!"
+$organization = "myOrga"
+$project = "myProj"
+
+$pipelineName = Read-Host "Please enter pipeline to delete"
+
+# Get all Azure Data Factory pipelines
+$url = "https://dev.azure.com/$organization/$project/_apis/pipelines?api-version=6.0"
+$pipelines = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json" -Headers $header
+
+$pipelines.value | ForEach-Object {
+    if ($_.name -eq $pipelineName) {
+        # Get the pipeline ID
+        $pipelineId = $_.id
+        
+        # Delete the pipeline
+        $url = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId?api-version=6.0"
+        Invoke-RestMethod -Uri $url -Method Delete -Headers $header
+
+        Write-Host "Pipeline '$pipelineName' deleted."
+
+        # Delete the associated dataset (if applicable)
+        $datasetUrl = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/datasets?api-version=6.0"
+        Invoke-RestMethod -Uri $datasetUrl -Method Delete -Headers $header
+        Write-Host "Dataset associated with pipeline '$pipelineName' deleted."
+
+        # Delete the linked service (if applicable)
+        $linkedServiceUrl = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/linkedServices?api-version=6.0"
+        Invoke-RestMethod -Uri $linkedServiceUrl -Method Delete -Headers $header
+        Write-Host "Linked service associated with pipeline '$pipelineName' deleted."
+
+        break
+    }
 }
 
-if ($ExitCode) {
-    exit 5
-}
-
-$Env:MyVariable
-$Parameter1
-$PSVersionTable
+Write-Host
