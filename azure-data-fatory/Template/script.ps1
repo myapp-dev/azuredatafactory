a83d19fe453fcf0486ad35d0d25329862d1f2774
rahul <azuredevopst@gmail.com>
2024-01-10 17:25:39 +0530

    code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -1,39 +1,24 @@
-$personalToken = "AzureDevOpsPersonalToken"
-$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalToken)"))
-$header = @{authorization = "Basic $token"}
-
-$organization = "myOrga"
-$project = "myProj"
-
-$pipelineName = Read-Host "Please enter pipeline to delete"
-
-# Get all Azure Data Factory pipelines
-$url = "https://dev.azure.com/$organization/$project/_apis/pipelines?api-version=6.0"
-$pipelines = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json" -Headers $header
-
-$pipelines.value | ForEach-Object {
-    if ($_.name -eq $pipelineName) {
-        # Get the pipeline ID
-        $pipelineId = $_.id
-        
-        # Delete the pipeline
-        $url = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId?api-version=6.0"
-        Invoke-RestMethod -Uri $url -Method Delete -Headers $header
-
-        Write-Host "Pipeline '$pipelineName' deleted."
-
-        # Delete the associated dataset (if applicable)
-        $datasetUrl = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/datasets?api-version=6.0"
-        Invoke-RestMethod -Uri $datasetUrl -Method Delete -Headers $header
-        Write-Host "Dataset associated with pipeline '$pipelineName' deleted."
-
-        # Delete the linked service (if applicable)
-        $linkedServiceUrl = "https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/linkedServices?api-version=6.0"
-        Invoke-RestMethod -Uri $linkedServiceUrl -Method Delete -Headers $header
-        Write-Host "Linked service associated with pipeline '$pipelineName' deleted."
-
-        break
-    }
+# Install the Azure PowerShell module if needed
+if (!(Get-Module -ListAvailable Az)) {
+    Install-Module Az -Scope CurrentUser
 }
 
-Write-Host
+# Connect to Azure
+Connect-AzAccount
+
+# Specify Data Factory and resource group names
+$dataFactoryName = "myappadfa"
+$resourceGroupName = "project-dev"
+
+# Retrieve Data Factory details
+$dataFactory = Get-AzDataFactory -ResourceGroupName $resourceGroupName -Name $dataFactoryName
+
+# Delete datasets
+foreach ($dataset in $dataFactory.Datasets) {
+    Remove-AzDataFactoryDataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $dataset.Name -Force  # Use -Force to potentially bypass dependencies
+}
+
+# Delete linked services
+foreach ($linkedService in $dataFactory.LinkedServices) {
+    Remove-AzDataFactoryLinkedService -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $linkedService.Name -Force  # Use -Force to potentially bypass dependencies
+}
