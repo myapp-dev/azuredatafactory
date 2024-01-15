23f54c2d63be0c197bf74a6923fce9c3466ce58f
rahul <azuredevopst@gmail.com>
2024-01-15 12:47:49 +0530

    pwsh code updated

--- azure-data-fatory/Template/script.ps1
+++ azure-data-fatory/Template/script.ps1
@@ -3,16 +3,18 @@ param(
     [parameter(Mandatory = $true)] [string]$dataFactoryName
 )
 
+# Check if AzureRM context is available, if not, log in
 if ([string]::IsNullOrEmpty($(Get-AzureRmContext).Account)) {
+    Write-Host "Logging in to AzureRM account..."
     Add-AzureRmAccount
 }
 
 $ADF_Triggers = Get-AzureRmDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $dataFactoryName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 Write-Host $ADF_Triggers.Name
+
 if ($notPresent) {
     Write-Host "Trigger does not exist. Nothing to enable!"
-}
-else {
+} else {
     $ADF_Triggers | ForEach-Object { 
         Write-Host "Enabling Pipeline Trigger $($_.name)"
         Start-AzureRmDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $dataFactoryName -Name $_.name -Force 
